61d2394ce488057f835d43c8b294a53000c37289
Rmichal503 <rakmichal503@gmail.com>
2023-10-13 17:53:00 +0200

    rpc querry for edit,delete and currentvalue

--- components/ExpensesButtons/DeleteCard.tsx
+++ components/ExpensesButtons/DeleteCard.tsx
@@ -6,25 +6,26 @@ import React from 'react'
 
 const supabase = createClientComponentClient<Database>()
 
-const deleteList = async (id: string,title:string) => {
-    const { error } = await supabase.from('spends').delete().eq('id', id)
-    if (error){
+const deleteList = async (id: string, title: string) => {
+    const { error } = await supabase.rpc('deletecard', { p_id: id, p_title: title })
+    // const { error } = await supabase.from('spends').delete().eq('id', id)
+    if (error) {
         console.error(error)
         return
-    } 
-    const {error:expenseError} = await supabase.from('expenses').delete().eq('title', title)
-    if (expenseError) console.error(expenseError)
+    }
+    // const {error:expenseError} = await supabase.from('expenses').delete().eq('title', title)
+    // if (expenseError) console.error(expenseError)
     setTimeout(() => {
         location.reload()
     }, 400)
 }
 
-export default function DeleteCard({id,title}:{id:string,title:string}) {
+export default function DeleteCard({ id, title }: { id: string, title: string }) {
     return (
         <div className='flex pt-2 justify-end'>
             <Button className='rounded-md' icon={Trash} variant='secondary' color='rose' onClick={(e) => {
                 e.preventDefault()
-                deleteList(id,title)
+                deleteList(id, title)
             }} >Delete List</Button>
         </div>
     )
